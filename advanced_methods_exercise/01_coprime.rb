f923e294017c1b162e9b568cfa7b12baaf78482c
CGfrac <cgfrac@protonmail.com>
2020-10-14 09:13:06 +0200

    Solve problem 1

--- advanced_methods_exercise/01_coprime.rb
+++ advanced_methods_exercise/01_coprime.rb
@@ -3,6 +3,16 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def coprime?(num_1, num_2)
+    divisors_1 = divisors(num_1)
+    divisors_1.none? {|d| num_2 % d == 0}
+end
+  
+def divisors(num)
+    arr = []
+    (2...num).each { |i| arr << i if num % i == 0 }
+    return arr
+end
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
