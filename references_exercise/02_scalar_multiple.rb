1faf0a278080c39799e059e0298cd1c12573e7d7
CGfrac <cgfrac@protonmail.com>
2020-10-15 10:36:04 +0200

    Use array.map! for cleaner code

--- references_exercise/02_scalar_multiple.rb
+++ references_exercise/02_scalar_multiple.rb
@@ -7,8 +7,7 @@
 # to be the same before and after calling your method.
 
 def scalar_multiple!(array, multiplier)
-    (0...array.length).each { |i| array[i] *= multiplier}
-    array
+    array.map! { |num| num * multiplier }
 end
 
 
