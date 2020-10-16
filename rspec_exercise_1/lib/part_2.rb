cc35365aaf5589db050b7aea620c79c524c8bb37
CGfrac <cgfrac@protonmail.com>
2020-10-16 09:11:25 +0200

    Implement hipsterfy

---
+++ rspec_exercise_1/lib/part_2.rb
@@ -0,0 +1,12 @@
+def hipsterfy(word)
+    vowels = "aeiou"
+    i = word.length - 1
+    while i > 0
+        if vowels.include?(word[i])
+            word[i] = ""
+            break
+        end
+        i -= 1
+    end
+    word
+end
