06f24af17787969ad48d1aead16666e1eab91e79
CGfrac <cgfrac@protonmail.com>
2020-10-17 14:25:43 +0200

    Implement is_prime?

--- 01_software_engineering_foundations/07_debugging_exercise_2/lib/problems.rb
+++ 01_software_engineering_foundations/07_debugging_exercise_2/lib/problems.rb
@@ -1,3 +1,9 @@
 # Run `bundle exec rspec` and satisy the specs.
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
+
+def is_prime?(num)
+    (2...num).each { |i| return false if num % i == 0 }
+    true
+end
+
