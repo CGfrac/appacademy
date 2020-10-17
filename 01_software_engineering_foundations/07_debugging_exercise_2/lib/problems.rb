b67ff1b59ab065b22fa76b5ca634c3dfc866f9c8
CGfrac <cgfrac@protonmail.com>
2020-10-17 14:46:12 +0200

    Implement largest_prime_factor and prevent issues on prior methods

--- 01_software_engineering_foundations/07_debugging_exercise_2/lib/problems.rb
+++ 01_software_engineering_foundations/07_debugging_exercise_2/lib/problems.rb
@@ -2,7 +2,10 @@
 # You should implement your methods in this file.
 # Feel free to use the debugger when you get stuck.
 
+require "byebug"
+
 def is_prime?(num)
+    return false if num < 2
     (2...num).each { |i| return false if num % i == 0 }
     true
 end
@@ -10,11 +13,22 @@ end
 def divisors(num)
     factors = []
     i = 2
-    while i < num
+    while i <= num
         if num % i == 0
             factors << i
         end
         i += 1
     end
     factors
+end
+
+def largest_prime_factor(num)
+    candidates = divisors(num)
+    i = candidates.length - 1
+    while i >= 0 
+        n = candidates[i]
+        return n if is_prime?(n)
+        i -= 1
+    end
+    1
 end
