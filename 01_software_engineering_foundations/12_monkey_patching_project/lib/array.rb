3284095262051350511e161cb0fe27a91decb107
CGfrac <cgfrac@protonmail.com>
2020-10-20 09:22:39 +0200

    Implement Array#span

--- 01_software_engineering_foundations/12_monkey_patching_project/lib/array.rb
+++ 01_software_engineering_foundations/12_monkey_patching_project/lib/array.rb
@@ -1,4 +1,11 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+  def span
+    if self.length > 0
+        minmax = self.minmax
+        minmax[1] - minmax[0]
+    else
+        return nil
+    end
+  end
 end
