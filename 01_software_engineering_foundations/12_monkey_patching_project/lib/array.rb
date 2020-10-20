3152a987a2560a817d06f20942e90ab14efc8083
CGfrac <cgfrac@protonmail.com>
2020-10-20 09:25:05 +0200

    Implement Array#average

--- 01_software_engineering_foundations/12_monkey_patching_project/lib/array.rb
+++ 01_software_engineering_foundations/12_monkey_patching_project/lib/array.rb
@@ -1,11 +1,19 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  def span
-    if self.length > 0
-        minmax = self.minmax
-        minmax[1] - minmax[0]
-    else
-        return nil
+    def span
+        if self.length > 0
+            minmax = self.minmax
+            minmax[1] - minmax[0]
+        else
+            return nil
+        end
     end
-  end
+
+    def average
+        if self.length > 0
+            self.sum / (self.length * 1.0)
+        else
+            return nil
+        end
+    end
 end
