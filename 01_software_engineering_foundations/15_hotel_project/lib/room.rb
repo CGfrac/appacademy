bb43a61d546d3a9ddc741abbab399ce1b4551735
CGfrac <cgfrac@protonmail.com>
2020-10-21 14:13:36 +0200

    Implement class initialization and getters

--- 01_software_engineering_foundations/15_hotel_project/lib/room.rb
+++ 01_software_engineering_foundations/15_hotel_project/lib/room.rb
@@ -1,3 +1,14 @@
 class Room
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
 
+    def capacity
+        @capacity
+    end
+
+    def occupants
+        @occupants
+    end
 end
