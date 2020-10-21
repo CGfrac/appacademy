846c6cd17dc2101601c4d832cc3e8754c077fd73
CGfrac <cgfrac@protonmail.com>
2020-10-21 14:36:03 +0200

    Implement class initialization

--- 01_software_engineering_foundations/15_hotel_project/lib/hotel.rb
+++ 01_software_engineering_foundations/15_hotel_project/lib/hotel.rb
@@ -1,5 +1,9 @@
 require_relative "room"
 
 class Hotel
-  
+    def initialize(name, rooms)
+        @name = name
+        @rooms = {}
+        rooms.each { |name, capacity| @rooms[name] = Room.new(capacity) }
+    end
 end
