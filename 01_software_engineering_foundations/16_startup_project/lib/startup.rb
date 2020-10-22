4f4781762efe5744321f2f487fd00282dd019125
CGfrac <cgfrac@protonmail.com>
2020-10-22 09:28:55 +0200

    Implement #hire

--- 01_software_engineering_foundations/16_startup_project/lib/startup.rb
+++ 01_software_engineering_foundations/16_startup_project/lib/startup.rb
@@ -17,4 +17,12 @@ class Startup
     def >(startup)
         self.funding > startup.funding
     end
+
+    def hire(employee_name, title)
+        if self.valid_title?(title)
+            employees << Employee.new(employee_name, title)
+        else
+            raise "Invalid title"
+        end
+    end    
 end
