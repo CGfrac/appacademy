610b67358c197c9921985e4b511ca9ac93a3866c
CGfrac <cgfrac@protonmail.com>
2020-10-23 09:47:55 +0200

    Implement #print_matches

--- 01_software_engineering_foundations/17_mastermind_project/lib/mastermind.rb
+++ 01_software_engineering_foundations/17_mastermind_project/lib/mastermind.rb
@@ -4,4 +4,9 @@ class Mastermind
   def initialize(length)
     @secret_code = Code.random(length)
   end
+
+  def print_matches(guess)
+    puts "Exact matches: #{@secret_code.num_exact_matches(guess)}"
+    puts "Near matches: #{@secret_code.num_near_matches(guess)}"
+  end
 end
