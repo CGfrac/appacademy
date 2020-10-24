4dd7f71385eda0fe1789e43448a38bc5735966df
CGfrac <cgfrac@protonmail.com>
2020-10-24 13:02:41 +0200

    Implement getters

--- 01_software_engineering_foundations/18_battleship_project/lib/battleship.rb
+++ 01_software_engineering_foundations/18_battleship_project/lib/battleship.rb
@@ -2,6 +2,8 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+    attr_reader :player, :board
+    
     def initialize(n)
         @player = Player.new()
         @board = Board.new(n)
