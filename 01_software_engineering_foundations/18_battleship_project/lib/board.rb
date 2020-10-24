c0dca834922d98bcb0579fb9c0afc07646c26b80
CGfrac <cgfrac@protonmail.com>
2020-10-24 10:18:04 +0200

    Implement #[] and #[]=

--- 01_software_engineering_foundations/18_battleship_project/lib/board.rb
+++ 01_software_engineering_foundations/18_battleship_project/lib/board.rb
@@ -1,8 +1,20 @@
 class Board
   attr_reader :size
-  
+
   def initialize(n)
     @grid = Array.new(n) { Array.new(n, :N) }
     @size = n * n
+  end
+
+  def [](pos)
+    row = pos[0]
+    col = pos[1]
+    @grid[row][col]
+  end
+
+  def []=(pos, value)
+    row = pos[0]
+    col = pos[1]
+    @grid[row][col] = value
   end
 end
