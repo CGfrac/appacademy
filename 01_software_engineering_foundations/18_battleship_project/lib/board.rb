d1692b8d0744ecc5b34445d8585266631bf421dd
CGfrac <cgfrac@protonmail.com>
2020-10-24 11:18:29 +0200

    Make indentation consistent

--- 01_software_engineering_foundations/18_battleship_project/lib/board.rb
+++ 01_software_engineering_foundations/18_battleship_project/lib/board.rb
@@ -1,52 +1,52 @@
 class Board
-  attr_reader :size
+    attr_reader :size
 
-  def initialize(n)
-    @grid = Array.new(n) { Array.new(n, :N) }
-    @size = n * n
-  end
-
-  def [](pos)
-    row = pos[0]
-    col = pos[1]
-    @grid[row][col]
-  end
-
-  def []=(pos, value)
-    row = pos[0]
-    col = pos[1]
-    @grid[row][col] = value
-  end
-
-  def num_ships
-    count = 0
-    @grid.each { |row| count += row.count(:S) }
-    count
-  end
-
-  def attack(pos)
-    if self[pos] == :S
-        self[pos] = :H
-        puts "you sunk my battleship!"
-        return true
-    else
-        self[pos] = :X
-        return false
+    def initialize(n)
+        @grid = Array.new(n) { Array.new(n, :N) }
+        @size = n * n
     end
-  end
 
-  def place_random_ships
-    n = @grid[0].length
-    max_ships = @size / 4
-    ships = 0
-    while ships < max_ships
-        row = rand(0...n)
-        col = rand(0...n)
-        pos = [row, col]
-        if self[pos] != :S
-            self[pos] = :S
-            ships += 1
+    def [](pos)
+        row = pos[0]
+        col = pos[1]
+        @grid[row][col]
+    end
+
+    def []=(pos, value)
+        row = pos[0]
+        col = pos[1]
+        @grid[row][col] = value
+    end
+
+    def num_ships
+        count = 0
+        @grid.each { |row| count += row.count(:S) }
+        count
+    end
+
+    def attack(pos)
+        if self[pos] == :S
+            self[pos] = :H
+            puts "you sunk my battleship!"
+            return true
+        else
+            self[pos] = :X
+            return false
         end
     end
-  end
+
+    def place_random_ships
+        n = @grid[0].length
+        max_ships = @size / 4
+        ships = 0
+        while ships < max_ships
+            row = rand(0...n)
+            col = rand(0...n)
+            pos = [row, col]
+            if self[pos] != :S
+                self[pos] = :S
+                ships += 1
+            end
+        end
+    end
 end
