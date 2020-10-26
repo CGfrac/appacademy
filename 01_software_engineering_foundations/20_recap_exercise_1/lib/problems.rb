7bfbd9d248055138b010f359b7f6e9e600eb4de2
CGfrac <cgfrac@protonmail.com>
2020-10-26 09:07:51 +0100

    Solve Hash#my_select

--- 01_software_engineering_foundations/20_recap_exercise_1/lib/problems.rb
+++ 01_software_engineering_foundations/20_recap_exercise_1/lib/problems.rb
@@ -58,7 +58,8 @@ class Hash
     # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
     # hash_2.my_select                            # => {4=>4}
     def my_select(&prc)
-
+        prc ||= Proc.new { |k, v| k == v }
+        self.select { |k, v| prc.call(k, v) }
     end
 end
 
