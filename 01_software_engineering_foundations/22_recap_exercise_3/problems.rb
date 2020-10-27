1fbd047e0172aa0683f37973afbd6b04a9491233
CGfrac <cgfrac@protonmail.com>
2020-10-27 09:56:41 +0100

    Solve String#select

--- 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
+++ 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
@@ -175,3 +175,23 @@ p vowel_rotate('headphones')    # => "heedphanos"
 p vowel_rotate('bootcamp')      # => "baotcomp"
 p vowel_rotate('awesome')       # => "ewasemo"
 puts "---------------------------------"
+
+# Extend the string class by defining a String#select method that accepts a block.
+# The method should return a new string containing characters of the original string that return true when passed into the block.
+# If no block is passed, then return the empty string. Do not use the built-in Array#select in your solution.
+
+class String
+    def select(&prc)
+        prc ||= Proc.new { false }
+        new_string = ""
+        self.each_char { |char| new_string += char if prc.call(char) }
+        new_string
+    end
+end
+
+puts "String#select"
+puts "---------------------------------"
+p "app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
+p "HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
+p "HELLOworld".select          # => ""
+puts "---------------------------------"
