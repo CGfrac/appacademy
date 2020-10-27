c60793330d5555ca8092f1402c6bf4e23c19f4ca
CGfrac <cgfrac@protonmail.com>
2020-10-27 08:57:28 +0100

    Solve no_consecutive_repeats?

--- 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
+++ 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
@@ -12,3 +12,23 @@ p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
 p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
 p no_dupes?([true, true, true])            # => []
 p "---------------------------------"
+
+# Write a method no_consecutive_repeats?(arr) that accepts an array as an arg. 
+# The method should return true if an element never appears consecutively in the array; it should return false otherwise.
+
+def no_consecutive_repeats?(arr)
+    prev = nil
+    arr.each do |ele|
+        return false if ele == prev
+        prev = ele
+    end
+    true
+end
+
+p "---------------------------------"
+p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+p no_consecutive_repeats?(['x'])                              # => true
+p "---------------------------------"
