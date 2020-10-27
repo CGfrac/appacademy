2d4c3aa3286d37e163cef821a39ef9b688745275
CGfrac <cgfrac@protonmail.com>
2020-10-27 09:42:16 +0100

    Solve vigenere_cipher

--- 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
+++ 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
@@ -111,3 +111,39 @@ p bi_prime?(94)   # => true
 p bi_prime?(24)   # => false
 p bi_prime?(64)   # => false
 puts "---------------------------------"
+
+# A Caesar cipher takes a word and encrypts it by offsetting each letter in the word by a fixed number, called the key.
+# Given a key of 3, for example: a -> d, p -> s, and y -> b.
+
+# A Vigenere Cipher is a Caesar cipher, but instead of a single key, a sequence of keys is used.
+# For example, if we encrypt "bananasinpajamas" with the key sequence 1, 2, 3, then the result would be "ccqbpdtkqqcmbodt":
+
+# Message:  b a n a n a s i n p a j a m a s
+# Keys:     1 2 3 1 2 3 1 2 3 1 2 3 1 2 3 1
+# Result:   c c q b p d t k q q c m b o d t
+
+# Write a method vigenere_cipher(message, keys) that accepts a string and a key-sequence as args, returning the encrypted message.
+# Assume that the message consists of only lowercase alphabetic characters.
+
+def vigenere_cipher(message, keys)
+    alphabet = ("a".."z").to_a
+    rotation = 0
+    cipher = ""
+
+    message.each_char do |char|
+        index = (alphabet.index(char) + keys[rotation]) % 26
+        cipher += alphabet[index]
+        rotation = (rotation + 1) % keys.length
+    end
+
+    cipher
+end
+
+puts "vigenere_cipher"
+puts "---------------------------------"
+p vigenere_cipher("toerrishuman", [1])        # => "upfssjtivnbo"
+p vigenere_cipher("toerrishuman", [1, 2])     # => "uqftsktjvobp"
+p vigenere_cipher("toerrishuman", [1, 2, 3])  # => "uqhstltjxncq"
+p vigenere_cipher("zebra", [3, 0])            # => "ceerd"
+p vigenere_cipher("yawn", [5, 1])             # => "dbbo"
+puts "---------------------------------"
