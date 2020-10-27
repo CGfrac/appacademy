51810ebb96bb874989d32a83919ef4c41f33a8f9
CGfrac <cgfrac@protonmail.com>
2020-10-27 09:35:12 +0100

    Solve bi_prime?

--- 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
+++ 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
@@ -76,3 +76,38 @@ p longest_streak('aaaxyyyyyzz') # => 'yyyyy
 p longest_streak('aaabbb')      # => 'bbb'
 p longest_streak('abc')         # => 'c'
 puts "---------------------------------"
+
+# Write a method bi_prime?(num) that accepts a number as an arg and returns a boolean indicating whether or not the number is a bi-prime.
+# A bi-prime is a positive integer that can be obtained by multiplying two prime numbers.
+
+# For Example:
+
+#     14 is a bi-prime because 2 * 7
+#     22 is a bi-prime because 2 * 11
+#     25 is a bi-prime because 5 * 5
+#     24 is not a bi-prime because no two prime numbers have a product of 24
+
+def bi_prime?(num)
+    i = 2
+    while i < (num / 2)
+        return prime?(num / i) if prime?(i) && num % i == 0
+        i += 1
+    end
+    false
+end
+
+def prime?(num)
+    return false if num < 2
+    (2..Integer.sqrt(num)).each { |i| return false if num % i == 0 }
+    true
+end
+
+puts "bi_prime?"
+puts "---------------------------------"
+p bi_prime?(14)   # => true
+p bi_prime?(22)   # => true
+p bi_prime?(25)   # => true
+p bi_prime?(94)   # => true
+p bi_prime?(24)   # => false
+p bi_prime?(64)   # => false
+puts "---------------------------------"
