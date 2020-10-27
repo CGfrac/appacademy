19a57064e4f251fa84f5f5ab6d7679e345cb89f6
CGfrac <cgfrac@protonmail.com>
2020-10-27 08:54:00 +0100

    Set format for solution output

--- 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
+++ 01_software_engineering_foundations/22_recap_exercise_3/problems.rb
@@ -6,6 +6,9 @@ def no_dupes?(arr)
     counter.keys.select { |k| counter[k] == 1 }
 end
 
+p "---------------------------------"
+p "no_dupes?"
 p no_dupes?([1, 1, 2, 1, 3, 2, 4])         # => [3, 4]
 p no_dupes?(['x', 'x', 'y', 'z', 'z'])     # => ['y']
 p no_dupes?([true, true, true])            # => []
+p "---------------------------------"
