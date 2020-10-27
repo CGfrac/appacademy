da361738b6fe2462890a62c8b2d5c4cb77f7602a
CGfrac <cgfrac@protonmail.com>
2020-10-27 11:09:36 +0100

    Solve lucas_sequence

--- 01_software_engineering_foundations/22_recap_exercise_3/recursion_problems.rb
+++ 01_software_engineering_foundations/22_recap_exercise_3/recursion_problems.rb
@@ -26,3 +26,31 @@ p multiply(-3, -6)      # => 18
 p multiply(3, -6)       # => -18
 p multiply(-3, 6)       # => -18
 puts "---------------------------------"
+
+# The Lucas Sequence is a sequence of numbers. 
+# The first number of the sequence is 2. 
+# The second number of the Lucas Sequence is 1. 
+# To generate the next number of the sequence, we add up the previous two numbers.
+# For example, the first six numbers of the sequence are: 2, 1, 3, 4, 7, 11, ...
+# 
+# Write a method lucasSequence that accepts a number representing a length as an arg.
+# The method should return an array containing the Lucas Sequence up to the given length. 
+# Solve this recursively.
+
+def lucas_sequence(n)
+    return [] if n == 0
+    return [2] if n == 1
+    sequence = lucas_sequence(n - 1)
+    return sequence + [1] if n == 2
+    sequence << sequence[-2] + sequence[-1]
+end
+
+puts "lucas_sequence"
+puts "---------------------------------"
+p lucas_sequence(0)   # => []
+p lucas_sequence(1)   # => [2]    
+p lucas_sequence(2)   # => [2, 1]
+p lucas_sequence(3)   # => [2, 1, 3]
+p lucas_sequence(6)   # => [2, 1, 3, 4, 7, 11]
+p lucas_sequence(8)   # => [2, 1, 3, 4, 7, 11, 18, 29]
+puts "---------------------------------"
