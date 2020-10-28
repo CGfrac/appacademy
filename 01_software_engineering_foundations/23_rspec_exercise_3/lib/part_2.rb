f5caf39e6789b874c886013eeaee25dacd880a65
CGfrac <cgfrac@protonmail.com>
2020-10-28 10:31:59 +0100

    Implement product_inject

--- 01_software_engineering_foundations/23_rspec_exercise_3/lib/part_2.rb
+++ 01_software_engineering_foundations/23_rspec_exercise_3/lib/part_2.rb
@@ -8,3 +8,7 @@ def char_replace!(str, hash)
     str.each_char.with_index { |ch, i| str[i] = hash[ch] if hash.has_key?(ch) }
     str
 end
+
+def product_inject(arr)
+    arr.inject { |product, num| product *= num}
+end
