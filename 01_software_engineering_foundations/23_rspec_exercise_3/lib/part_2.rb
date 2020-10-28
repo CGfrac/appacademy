03e9d3d012bd655be8730ef7d6b556190c531a71
CGfrac <cgfrac@protonmail.com>
2020-10-28 10:26:48 +0100

    Implement char_replace!

--- 01_software_engineering_foundations/23_rspec_exercise_3/lib/part_2.rb
+++ 01_software_engineering_foundations/23_rspec_exercise_3/lib/part_2.rb
@@ -3,3 +3,8 @@ def element_count(arr)
     arr.each { |ele| counter[ele] += 1 }
     counter
 end
+
+def char_replace!(str, hash)
+    str.each_char.with_index { |ch, i| str[i] = hash[ch] if hash.has_key?(ch) }
+    str
+end
