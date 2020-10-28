07fae2bb86890b372e262334e15454d8f6e1bc91
CGfrac <cgfrac@protonmail.com>
2020-10-28 10:53:55 +0100

    Implement hash_select

--- 01_software_engineering_foundations/24_rspec_exercise_4/lib/part_1.rb
+++ 01_software_engineering_foundations/24_rspec_exercise_4/lib/part_1.rb
@@ -5,3 +5,9 @@ end
 def my_one?(arr, &prc)
     return arr.count { |ele| prc.call(ele) } == 1
 end
+
+def hash_select(hash, &prc)
+    new_hash = {}
+    hash.each { |k, v| new_hash[k] = v if prc.call(k, v) }
+    new_hash
+end
