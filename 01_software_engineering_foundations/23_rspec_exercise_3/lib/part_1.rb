228a8955f8f47246f1b7502f54be7bb58b656dae
CGfrac <cgfrac@protonmail.com>
2020-10-28 10:16:06 +0100

    Implement nth_prime

--- 01_software_engineering_foundations/23_rspec_exercise_3/lib/part_1.rb
+++ 01_software_engineering_foundations/23_rspec_exercise_3/lib/part_1.rb
@@ -3,3 +3,14 @@ def is_prime?(num)
     (2...num).each { |i| return false if num % i == 0 }
     true
 end
+
+def nth_prime(n)
+    return 2 if n == 1
+    i = 3
+    counter = 2
+    while counter < n
+        i += 2
+        counter += 1 if is_prime?(i)
+    end
+    i
+end
