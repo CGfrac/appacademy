e8580e1fc4aa825ffd7d4e8ede753a6ad7886aca
CGfrac <cgfrac@protonmail.com>
2020-10-28 11:16:00 +0100

    Implement prizz_proc

--- 01_software_engineering_foundations/25_rspec_exercise_5/lib/exercise.rb
+++ 01_software_engineering_foundations/25_rspec_exercise_5/lib/exercise.rb
@@ -1,3 +1,7 @@
 def zip(*arrs)
     arrs.transpose
 end
+
+def prizz_proc(arr, prc_1, prc_2)
+    arr.select { |ele| (prc_1.call(ele) && !prc_2.call(ele)) || (!prc_1.call(ele) && prc_2.call(ele)) }
+end
