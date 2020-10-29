4005947ec12e31c4c7149f52ac3e4f658e744341
CGfrac <cgfrac@protonmail.com>
2020-10-29 10:57:00 +0100

    Implement rampant_repeats

--- 01_software_engineering_foundations/26_nauseating_numbers/phase1.rb
+++ 01_software_engineering_foundations/26_nauseating_numbers/phase1.rb
@@ -44,3 +44,21 @@ p pair_product([3, 4, 6, 2, 5], 12) # true
 p pair_product([4, 2, 5, 7], 16)    # false
 p pair_product([8, 4, 9, 3], 8)     # false
 p pair_product([3], 12)             # false
+
+# Write a method rampant_repeats that accepts a string and a hash as arguments.
+# The method should return a new string where characters of the original string are repeated the number of times specified by the hash.
+# If a character does not exist as a key of the hash, then it should remain unchanged.
+
+def rampant_repeats(str, hash)
+    new_str = ""
+    str.each_char { |char| new_str << char * (hash[char] || 1) }
+    new_str
+end
+
+puts "-----------------------------------"
+puts "rampant_repeats"
+puts "-----------------------------------"
+p rampant_repeats('taco', {'a'=>3, 'c'=>2})             # 'taaacco'
+p rampant_repeats('feverish', {'e'=>2, 'f'=>4, 's'=>3}) # 'ffffeeveerisssh'
+p rampant_repeats('misispi', {'s'=>2, 'p'=>2})          # 'mississppi'
+p rampant_repeats('faarm', {'e'=>3, 'a'=>2})            # 'faaaarm'
