2a898ac116684ad967b5e23569eaf6a3683a7496
CGfrac <cgfrac@protonmail.com>
2020-10-29 09:44:28 +0100

    Implement zany_zip

--- 01_software_engineering_foundations/25_rspec_exercise_5/lib/exercise.rb
+++ 01_software_engineering_foundations/25_rspec_exercise_5/lib/exercise.rb
@@ -5,3 +5,19 @@ end
 def prizz_proc(arr, prc_1, prc_2)
     arr.select { |ele| (prc_1.call(ele) && !prc_2.call(ele)) || (!prc_1.call(ele) && prc_2.call(ele)) }
 end
+
+require "byebug"
+
+def zany_zip(*arrs)
+    length = 0
+    arrs.each { |row| length = row.length if row.length > length }
+    new_arr = Array.new(length) { Array.new(arrs.length, nil) }
+    
+    (0...arrs.length).each do |i|
+        (0...arrs[i].length).each do |j|
+            new_arr[j][i] = arrs[i][j]
+        end
+    end
+
+    new_arr
+end
