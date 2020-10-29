66ce894342521d227db87a8fc539341f94443427
CGfrac <cgfrac@protonmail.com>
2020-10-29 11:17:11 +0100

    Implement matrix_addition

--- 01_software_engineering_foundations/26_nauseating_numbers/phase2.rb
+++ 01_software_engineering_foundations/26_nauseating_numbers/phase2.rb
@@ -31,3 +31,39 @@ p anti_prime?(5)    # false
 p anti_prime?(100)  # false
 p anti_prime?(136)  # false
 p anti_prime?(1024) # false
+
+# Let a 2-dimensional array be known as a "matrix".
+# Write a method matrix_addition that accepts two matrices as arguments.
+# The two matrices are guaranteed to have the same "height" and "width".
+# The method should return a new matrix representing the sum of the two arguments.
+# To add matrices, we simply add the values at the same positions:
+#  programmatically
+# [[2, 5], [4, 7]] + [[9, 1], [3, 0]] => [[11, 6], [7, 7]]
+
+#  structurally
+# 2 5  +  9 1  =>  11 6
+# 4 7     3 0      7 7
+
+def matrix_addition(matrix_1, matrix_2)
+    new_matrix = Array.new(matrix_1.length) { Array.new(matrix_1[0].length) }
+    (0...matrix_1.length).each do |i|
+        (0...matrix_1[0].length).each do |j|
+            new_matrix[i][j] = matrix_1[i][j] + matrix_2[i][j]
+        end
+    end
+    new_matrix
+end
+
+matrix_a = [[2,5], [4,7]]
+matrix_b = [[9,1], [3,0]]
+matrix_c = [[-1,0], [0,-1]]
+matrix_d = [[2, -5], [7, 10], [0, 1]]
+matrix_e = [[0 , 0], [12, 4], [6,  3]]
+
+puts "-----------------------------------"
+puts "matrix_addition"
+puts "-----------------------------------"
+p matrix_addition(matrix_a, matrix_b) # [[11, 6], [7, 7]]
+p matrix_addition(matrix_a, matrix_c) # [[1, 5], [4, 6]]
+p matrix_addition(matrix_b, matrix_c) # [[8, 1], [3, -1]]
+p matrix_addition(matrix_d, matrix_e) # [[2, -5], [19, 14], [6, 4]]
