45474d30cdfda11a1e16d1927261958606b2c6a6
CGfrac <cgfrac@protonmail.com>
2020-11-02 11:22:22 +0100

    Implement reverberate

--- 01_software_engineering_foundations/28_meddling_medley/phase2.rb
+++ 01_software_engineering_foundations/28_meddling_medley/phase2.rb
@@ -61,3 +61,41 @@ p convert_pig_latin('I cannot find the trash') # "I annotcay indfay ethay ashtra
 p convert_pig_latin('What an interesting problem') # "Atwhay an interestingyay oblempray"
 p convert_pig_latin('Her family flew to France') # "Erhay amilyfay ewflay to Ancefray"
 p convert_pig_latin('Our family flew to France') # "Ouryay amilyfay ewflay to Ancefray"
+
+# Write a method reverberate that accepts a sentence as an argument. 
+# The method should translate the sentence according to the following rules:
+
+#     words that are shorter than 3 characters are unchanged
+#     words that are 3 characters or longer are translated according to the following rules:
+#         if the word ends with a vowel, simply repeat the word twice (example: 'like'->'likelike')
+#         if the word ends with a non-vowel, repeat all letters that come after the word's last vowel, including the last vowel itself
+#             (example: 'trash'->'trashash')
+
+# Note that if words are capitalized in the original sentence, they should remain capitalized in the translated sentence. 
+# Vowels are the letters a, e, i, o, u.
+
+def reverberate(sentence)
+    new_sentence = []
+    sentence.split.each do |word|
+        if word.length < 3
+            new_sentence << word
+        elsif VOWELS.include?(word[-1])
+            new_sentence << word + word.downcase
+        else
+            i = word.length - 1
+            while !VOWELS.include?(word[i])
+                i -= 1
+            end
+            new_sentence << word + word[i..-1]
+        end
+    end
+    new_sentence.join(" ")
+end
+
+puts "-----------------------------------"
+puts "reverberate"
+puts "-----------------------------------"
+p reverberate('We like to go running fast') # "We likelike to go runninging fastast"
+p reverberate('He cannot find the trash') # "He cannotot findind thethe trashash"
+p reverberate('Pasta is my favorite dish') # "Pastapasta is my favoritefavorite dishish"
+p reverberate('Her family flew to France') # "Herer familyily flewew to Francefrance"
