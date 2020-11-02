6b5ba1e2ab119b634c310ec828abe2996c8cf4da
CGfrac <cgfrac@protonmail.com>
2020-11-02 10:16:17 +0100

    Implement duos

---
+++ 01_software_engineering_foundations/28_meddling_medley/phase1.rb
@@ -0,0 +1,17 @@
+# Write a method duos that accepts a string as an argument and returns the 
+# count of the number of times the same character appears consecutively in the given string.
+
+def duos(str)
+    count = 0
+    (1...str.length).each { |i| count += 1 if str[i] == str[i - 1] }
+    count
+end
+
+puts "-----------------------------------"
+puts "duos"
+puts "-----------------------------------"
+p duos('bootcamp')      # 1
+p duos('wxxyzz')        # 2
+p duos('hoooraay')      # 3
+p duos('dinosaurs')     # 0
+p duos('e')             # 0
