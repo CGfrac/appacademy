97b6a44ccc131dfd15535e69c2ca66b4fc483640
CGfrac <cgfrac@protonmail.com>
2020-11-02 11:26:07 +0100

    Refactor disjunct_select

--- 01_software_engineering_foundations/28_meddling_medley/phase2.rb
+++ 01_software_engineering_foundations/28_meddling_medley/phase2.rb
@@ -98,3 +98,34 @@ p reverberate('We like to go running fast') # "We likelike to go runninging fast
 p reverberate('He cannot find the trash') # "He cannotot findind thethe trashash"
 p reverberate('Pasta is my favorite dish') # "Pastapasta is my favoritefavorite dishish"
 p reverberate('Her family flew to France') # "Herer familyily flewew to Francefrance"
+
+# Write a method disjunct_select that accepts an array and one or more procs as arguments. 
+# The method should return a new array containing the elements that return true when passed into at least one of the given procs.
+
+def disjunct_select(arr, *prcs)
+    arr.select do |ele|
+        prcs.any? { |prc| prc.call(ele) }
+    end
+end
+
+puts "-----------------------------------"
+puts "disjunct_select"
+puts "-----------------------------------"
+longer_four = Proc.new { |s| s.length > 4 }
+contains_o = Proc.new { |s| s.include?('o') }
+starts_a = Proc.new { |s| s[0] == 'a' }
+
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+) # ["apple", "teeming"]
+
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+    contains_o
+) # ["dog", "apple", "teeming", "boot"]
+
+p disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+    longer_four,
+    contains_o,
+    starts_a
+) # ["ace", "dog", "apple", "teeming", "boot"]
