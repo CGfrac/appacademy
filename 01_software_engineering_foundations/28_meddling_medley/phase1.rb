b6a0774b46d0048404fc9d275b995e3a83c34ed6
CGfrac <cgfrac@protonmail.com>
2020-11-02 10:22:36 +0100

    Implement sentence_swap

--- 01_software_engineering_foundations/28_meddling_medley/phase1.rb
+++ 01_software_engineering_foundations/28_meddling_medley/phase1.rb
@@ -15,3 +15,33 @@ p duos('wxxyzz')        # 2
 p duos('hoooraay')      # 3
 p duos('dinosaurs')     # 0
 p duos('e')             # 0
+
+# Write a method sentence_swap that accepts a sentence and a hash as arguments. 
+# The method should return a new sentence where every word is replaced with it's corresponding value in the hash. 
+# If a word does not exist as a key of the hash, then it should remain unchanged.
+
+def sentence_swap(sentence, hash)
+    new_sentence = sentence.split.map do |word|
+        if hash.has_key?(word)
+            hash[word]
+        else
+            word
+        end
+    end
+    new_sentence.join(" ")
+end
+
+puts "-----------------------------------"
+puts "sentence_swap"
+puts "-----------------------------------"
+p sentence_swap('anything you can do I can do',
+    'anything'=>'nothing', 'do'=>'drink', 'can'=>'shall'
+) # 'nothing you shall drink I shall drink'
+
+p sentence_swap('what a sad ad',
+    'cat'=>'dog', 'sad'=>'happy', 'what'=>'make'
+) # 'make a happy ad'
+
+p sentence_swap('keep coding okay',
+    'coding'=>'running', 'kay'=>'pen'
+) # 'keep running okay'
