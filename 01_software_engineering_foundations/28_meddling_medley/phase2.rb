537b43956990b35a0320ba54b9814759bbf87ea4
CGfrac <cgfrac@protonmail.com>
2020-11-03 09:02:53 +0100

    Implement silly_talk

--- 01_software_engineering_foundations/28_meddling_medley/phase2.rb
+++ 01_software_engineering_foundations/28_meddling_medley/phase2.rb
@@ -185,3 +185,37 @@ p alternating_vowel('panthers are great animals') # "pnthers ar grat animls"
 p alternating_vowel('running panthers are epic') # "rnning panthrs re epc"
 p alternating_vowel('code properly please') # "cde proprly plase"
 p alternating_vowel('my forecast predicts rain today') # "my forecst prdicts ran tday"
+
+# Write a method silly_talk that accepts a sentence as an argument. 
+# The method should translate each word of the sentence according to the following rules:
+
+#     if the word ends with a vowel, simply repeat that vowel at the end of the word (example: 'code'->'codee')
+#     if the word ends with a non-vowel, every vowel of the word should be followed by 'b' and that same vowel (example: 'siren'->'sibireben')
+
+# Note that if words are capitalized in the original sentence, they should remain capitalized in the translated sentence. 
+# Vowels are the letters a, e, i, o, u.
+
+def silly_talk(sentence)
+    new_sentence = []
+    sentence.split.each do |word|
+        if VOWELS.include?(word[-1])
+            new_sentence << word + word[-1]
+        else
+            new_word = ""
+            word.each_char do |char|
+                new_word += char
+                new_word += "b" + char.downcase if VOWELS.include?(char.downcase)
+            end
+            new_sentence << new_word
+        end
+    end
+    new_sentence.join(" ")
+end
+
+puts "-----------------------------------"
+puts "silly_talk"
+puts "-----------------------------------"
+p silly_talk('Kids like cats and dogs') # "Kibids likee cabats aband dobogs"
+p silly_talk('Stop that scooter') # "Stobop thabat scobooboteber"
+p silly_talk('They can code') # "Thebey caban codee"
+p silly_talk('He flew to Italy') # "Hee flebew too Ibitabaly"
