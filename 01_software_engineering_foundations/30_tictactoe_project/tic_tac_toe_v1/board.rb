ed0ac6cca085f2df998cc9b1188c0ea830132f85
CGfrac <cgfrac@protonmail.com>
2020-11-03 10:01:11 +0100

    Implement Board#valid?

--- 01_software_engineering_foundations/30_tictactoe_project/tic_tac_toe_v1/board.rb
+++ 01_software_engineering_foundations/30_tictactoe_project/tic_tac_toe_v1/board.rb
@@ -2,4 +2,8 @@ class Board
     def initialize
         @grid = Array.new(3) { Array.new(3, '_') }
     end
+
+    def valid?(position)
+        position.all? { |coord| coord >= 0 && coord < @grid.length }
+    end
 end
