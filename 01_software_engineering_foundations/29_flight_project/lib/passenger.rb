12a65fc677b4de1209583aec7ffa8019b20e0365
CGfrac <cgfrac@protonmail.com>
2020-11-03 09:25:29 +0100

    Implement #add_flight

--- 01_software_engineering_foundations/29_flight_project/lib/passenger.rb
+++ 01_software_engineering_foundations/29_flight_project/lib/passenger.rb
@@ -9,4 +9,8 @@ class Passenger
     def has_flight?(number)
         @flight_numbers.include?(number.upcase)
     end
+
+    def add_flight(number)
+        @flight_numbers << number.upcase if !has_flight?(number)
+    end
 end
