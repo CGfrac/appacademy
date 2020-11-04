1ca6de45aae6cbee77af5096b169459b2e58df87
CGfrac <cgfrac@protonmail.com>
2020-11-04 10:55:12 +0100

    Implement ComputerPlayer#get_position

--- 01_software_engineering_foundations/30_tictactoe_project/tic_tac_toe_v3/computer_player.rb
+++ 01_software_engineering_foundations/30_tictactoe_project/tic_tac_toe_v3/computer_player.rb
@@ -4,4 +4,10 @@ class ComputerPlayer
     def initialize(mark_value)
         @mark_value = mark_value
     end
+
+    def get_position(legal_positions)
+        position = legal_positions.sample
+        puts "#{@mark_value} playing at #{position[0]} #{position[1]}"
+        position
+    end
 end
