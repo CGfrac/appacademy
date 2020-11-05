bc9b501838ea678f19014fb0f1d079434b4ea8c8
CGfrac <cgfrac@protonmail.com>
2020-11-05 09:26:25 +0100

    Implement List#add_item

--- 01_software_engineering_foundations/31_todo_board_project/list.rb
+++ 01_software_engineering_foundations/31_todo_board_project/list.rb
@@ -1,8 +1,19 @@
+require_relative "item.rb"
+
 class List
     attr_accessor :label
 
     def initialize(label)
         @label = label
         @items = []
+    end
+
+    def add_item(title, deadline, description="")
+        if Item.valid_date?(deadline)
+            @items << Item.new(title, deadline, description)
+            return true
+        else
+            return false
+        end
     end
 end
