0dd5ee18d45d2ea03ee9d7b4016cc59b913987ba
CGfrac <cgfrac@protonmail.com>
2020-11-05 09:07:34 +0100

    Implement Item::valid_date?

---
+++ 01_software_engineering_foundations/31_todo_board_project/item.rb
@@ -0,0 +1,10 @@
+class Item
+    # Should use something more robust like Date but that's part of the assignment
+    def self.valid_date?(date_string)
+        elements = date_string.split('-')
+        return false if elements.length != 3
+        year, month, day = elements
+        return false if year.length != 4 || month.to_i < 1 || month.to_i > 12 || day.to_i < 1 || day.to_i > 31
+        true
+    end
+end
