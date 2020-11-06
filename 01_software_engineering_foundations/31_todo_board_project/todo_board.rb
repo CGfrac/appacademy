eb0854225ac83dda9465c05e5ca1f93e90f77588
CGfrac <cgfrac@protonmail.com>
2020-11-06 09:46:30 +0100

    Implement TodoBoard#get_command

--- 01_software_engineering_foundations/31_todo_board_project/todo_board.rb
+++ 01_software_engineering_foundations/31_todo_board_project/todo_board.rb
@@ -1,5 +1,33 @@
+require_relative "list.rb"
+
 class TodoBoard
     def initialize(label)
         @list = List.new(label)
+    end
+
+    def get_command
+        print "\nEnter a command: "
+        cmd, *args = gets.chomp.split
+
+        case cmd
+        when "mktodo"
+            @list.add_item(*args)
+        when "up"
+            @list.up(*args.map! { |arg| arg.to_i }  )
+        when "down"
+            @list.down(*args.map! { |arg| arg.to_i } )
+        when "swap"
+            @list.swap_index(*args.map! { |arg| arg.to_i } )
+        when "sort"
+            @list.sort_by_date!
+        when "priority"
+            @list.print_priority
+        when "quit"
+            return false
+        else
+            "Wrong command, did you type it correctly?"
+        end
+
+        true
     end
 end
