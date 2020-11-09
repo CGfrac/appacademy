9182e59e79430621f7180248a8ac540f3d0dbc2a
CGfrac <cgfrac@protonmail.com>
2020-11-09 11:08:12 +0100

    Implement Array#my_select

--- 02_ruby/01_enumerables/enumerables.rb
+++ 02_ruby/01_enumerables/enumerables.rb
@@ -13,6 +13,9 @@ class Array
     end
 end
 
+puts "-----------------------------------"
+puts "my_each"
+puts "-----------------------------------"
 return_value = [1, 2, 3].my_each do |num|
   puts num
 end.my_each do |num|
@@ -26,3 +29,22 @@ end
 #    3
 
 p return_value  # => [1, 2, 3]
+
+# Now extend the Array class to include my_select that takes a block and 
+# returns a new array containing only elements that satisfy the block. 
+# Use your my_each method!
+
+class Array
+    def my_select(&prc)
+        new_arr = []
+        self.my_each { |ele| new_arr << ele if prc.call(ele) }
+        new_arr
+    end
+end
+
+puts "-----------------------------------"
+puts "my_select"
+puts "-----------------------------------"
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
