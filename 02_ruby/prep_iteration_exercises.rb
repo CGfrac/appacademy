4b3df294dcfa4e8c6670ac1b41ca0edb2e294eb2
CGfrac <cgfrac@protonmail.com>
2020-11-10 10:28:28 +0100

    Implement Array#bubble_sort!

--- 02_ruby/prep_iteration_exercises.rb
+++ 02_ruby/prep_iteration_exercises.rb
@@ -50,6 +50,16 @@ end
 
 class Array
   def bubble_sort!(&prc)
+    sorted = false
+    while !sorted
+      sorted = true
+      (0...self.length-1).each do |i|
+        if self[i] > self[i+1]
+          sorted = false
+          self[i], self[i+1] = self[i+1], self[i]
+        end
+      end
+    end
   end
 
   def bubble_sort(&prc)
