5635cf973d106861c750998a56a3503dc4b7b27c
CGfrac <cgfrac@protonmail.com>
2020-11-10 09:21:44 +0100

    Implement Array#my_zip

--- 02_ruby/01_enumerables/enumerables.rb
+++ 02_ruby/01_enumerables/enumerables.rb
@@ -110,3 +110,33 @@ puts "-----------------------------------"
 puts "my_flatten"
 puts "-----------------------------------"
 p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+# Write my_zip to take any number of arguments. 
+# It should return a new array containing self.length elements. 
+# Each element of the new array should be an array with a length of the input arguments + 1 
+# and contain the merged elements at that index. 
+# If the size of any argument is less than self, nil is returned for that location.
+
+class Array
+    def my_zip(*elements)
+        zip = Array.new(self.length) { Array.new(elements.length + 1, nil) }
+        (0...zip.length).each do |i|
+            zip[i][0] = self[i]
+            (0...elements.length).each { |j| zip[i][j + 1] = elements[j][i] }
+        end
+        zip
+    end
+end
+
+puts "-----------------------------------"
+puts "my_zip"
+puts "-----------------------------------"
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
