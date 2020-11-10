a465042236c4f8ae9f3bdd0b3c1f79273c003d87
CGfrac <cgfrac@protonmail.com>
2020-11-10 10:16:14 +0100

    Refactor code for use as library

--- 02_ruby/lib/enumerables_array.rb
+++ 02_ruby/lib/enumerables_array.rb
@@ -1,7 +1,3 @@
-# Extend the Array class to include a method named my_each that takes a block, 
-# calls the block on every element of the array, and returns the original array. 
-# Do not use Enumerable's each method.
-
 class Array
     def my_each(&prc)
         i = 0
@@ -11,66 +7,19 @@ class Array
         end
         self
     end
-end
 
-puts "-----------------------------------"
-puts "my_each"
-puts "-----------------------------------"
-return_value = [1, 2, 3].my_each do |num|
-  puts num
-end.my_each do |num|
-  puts num
-end
-# => 1
-#    2
-#    3
-#    1
-#    2
-#    3
-
-p return_value  # => [1, 2, 3]
-
-# Now extend the Array class to include my_select that takes a block and 
-# returns a new array containing only elements that satisfy the block. 
-# Use your my_each method!
-
-class Array
     def my_select(&prc)
         new_arr = []
         self.my_each { |ele| new_arr << ele if prc.call(ele) }
         new_arr
     end
-end
 
-puts "-----------------------------------"
-puts "my_select"
-puts "-----------------------------------"
-a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
-
-# Write my_reject to take a block and return a new array 
-# excluding elements that satisfy the block.
-
-class Array
     def my_reject(&prc)
         new_arr = []
         self.my_each { |ele| new_arr << ele if !prc.call(ele) }
         new_arr
     end
-end
 
-puts "-----------------------------------"
-puts "my_reject"
-puts "-----------------------------------"
-a = [1, 2, 3]
-p a.my_reject { |num| num > 1 } # => [1]
-p a.my_reject { |num| num == 4 } # => [1, 2, 3]
-
-# Write my_any? to return true if any elements of the array satisfy 
-# the block and my_all? to return true only if all elements satisfy the block.
-
-class Array
     def my_any?(&prc)
         self.my_each { |ele| return true if prc.call(ele) }
         false
@@ -80,21 +29,7 @@ class Array
         self.my_each { |ele| return false if !prc.call(ele) }
         true
     end
-end
 
-puts "-----------------------------------"
-puts "my_any? and my_all?"
-puts "-----------------------------------"
-a = [1, 2, 3]
-p a.my_any? { |num| num > 1 } # => true
-p a.my_any? { |num| num == 4 } # => false
-p a.my_all? { |num| num > 1 } # => false
-p a.my_all? { |num| num < 4 } # => true
-
-# my_flatten should return all elements of the array into a new, one-dimensional array. 
-# Hint: use recursion!
-
-class Array
     def my_flatten
         return [] if self.length == 0
         if self[0].is_a?(Array)
@@ -104,20 +39,7 @@ class Array
         end
         new_arr + self[1..-1].my_flatten
     end
-end
 
-puts "-----------------------------------"
-puts "my_flatten"
-puts "-----------------------------------"
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
-
-# Write my_zip to take any number of arguments. 
-# It should return a new array containing self.length elements. 
-# Each element of the new array should be an array with a length of the input arguments + 1 
-# and contain the merged elements at that index. 
-# If the size of any argument is less than self, nil is returned for that location.
-
-class Array
     def my_zip(*elements)
         zip = Array.new(self.length) { Array.new(elements.length + 1, nil) }
         (0...zip.length).each do |i|
@@ -126,26 +48,7 @@ class Array
         end
         zip
     end
-end
 
-puts "-----------------------------------"
-puts "my_zip"
-puts "-----------------------------------"
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
-
-c = [10, 11, 12]
-d = [13, 14, 15]
-p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
-
-# Write a method my_rotate that returns a new array containing all the elements of the original array in a rotated order. 
-# By default, the array should rotate by one element. 
-# If a negative value is given, the array is rotated in the opposite direction.
-
-class Array
     def my_rotate(direction=1)
         rotated = self[0..-1]
         if direction > 0
@@ -155,40 +58,13 @@ class Array
         end
         rotated
     end
-end
 
-puts "-----------------------------------"
-puts "my_rotate"
-puts "-----------------------------------"
-a = [ "a", "b", "c", "d" ]
-p a.my_rotate         #=> ["b", "c", "d", "a"]
-p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
-p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
-p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
-
-# my_join returns a single string containing all the elements of the array, 
-# separated by the given string separator. 
-# If no separator is given, an empty string is used.
-
-class Array
     def my_join(separator="")
         string = ""
         self.my_each { |ele| string += ele.to_s + separator }
         string[0...-1]
     end
-end
 
-puts "-----------------------------------"
-puts "my_join"
-puts "-----------------------------------"
-a = [ "a", "b", "c", "d" ]
-p a.my_join         # => "abcd"
-p a.my_join("$")    # => "a$b$c$d"
-
-# Write a method that returns a new array containing all the elements of 
-# the original array in reverse order.
-
-class Array
     def my_reverse
         reversed = []
         i = self.length - 1
@@ -200,8 +76,118 @@ class Array
     end
 end
 
-puts "-----------------------------------"
-puts "my_reverse"
-puts "-----------------------------------"
-p [ "a", "b", "c" ].my_reverse   #=> ["c", "b", "a"]
-p [ 1 ].my_reverse               #=> [1]
+if __FILE__ == $PROGRAM_NAME
+    # Extend the Array class to include a method named my_each that takes a block, 
+    # calls the block on every element of the array, and returns the original array. 
+    # Do not use Enumerable's each method.
+
+    puts "-----------------------------------"
+    puts "my_each"
+    puts "-----------------------------------"
+    return_value = [1, 2, 3].my_each do |num|
+    puts num
+    end.my_each do |num|
+    puts num
+    end
+    # => 1
+    #    2
+    #    3
+    #    1
+    #    2
+    #    3
+
+    p return_value  # => [1, 2, 3]
+
+    # Now extend the Array class to include my_select that takes a block and 
+    # returns a new array containing only elements that satisfy the block. 
+    # Use your my_each method!
+
+    puts "-----------------------------------"
+    puts "my_select"
+    puts "-----------------------------------"
+    a = [1, 2, 3]
+    p a.my_select { |num| num > 1 } # => [2, 3]
+    p a.my_select { |num| num == 4 } # => []
+
+    # Write my_reject to take a block and return a new array 
+    # excluding elements that satisfy the block.
+
+    puts "-----------------------------------"
+    puts "my_reject"
+    puts "-----------------------------------"
+    a = [1, 2, 3]
+    p a.my_reject { |num| num > 1 } # => [1]
+    p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+    # Write my_any? to return true if any elements of the array satisfy 
+    # the block and my_all? to return true only if all elements satisfy the block.
+
+    puts "-----------------------------------"
+    puts "my_any? and my_all?"
+    puts "-----------------------------------"
+    a = [1, 2, 3]
+    p a.my_any? { |num| num > 1 } # => true
+    p a.my_any? { |num| num == 4 } # => false
+    p a.my_all? { |num| num > 1 } # => false
+    p a.my_all? { |num| num < 4 } # => true
+
+    # my_flatten should return all elements of the array into a new, one-dimensional array. 
+    # Hint: use recursion!
+
+    puts "-----------------------------------"
+    puts "my_flatten"
+    puts "-----------------------------------"
+    p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+    # Write my_zip to take any number of arguments. 
+    # It should return a new array containing self.length elements. 
+    # Each element of the new array should be an array with a length of the input arguments + 1 
+    # and contain the merged elements at that index. 
+    # If the size of any argument is less than self, nil is returned for that location.
+
+    puts "-----------------------------------"
+    puts "my_zip"
+    puts "-----------------------------------"
+    a = [ 4, 5, 6 ]
+    b = [ 7, 8, 9 ]
+    p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+    p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+    c = [10, 11, 12]
+    d = [13, 14, 15]
+    p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+    # Write a method my_rotate that returns a new array containing all the elements of the original array in a rotated order. 
+    # By default, the array should rotate by one element. 
+    # If a negative value is given, the array is rotated in the opposite direction.
+
+    puts "-----------------------------------"
+    puts "my_rotate"
+    puts "-----------------------------------"
+    a = [ "a", "b", "c", "d" ]
+    p a.my_rotate         #=> ["b", "c", "d", "a"]
+    p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+    p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+    p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
+
+    # my_join returns a single string containing all the elements of the array, 
+    # separated by the given string separator. 
+    # If no separator is given, an empty string is used.
+
+    puts "-----------------------------------"
+    puts "my_join"
+    puts "-----------------------------------"
+    a = [ "a", "b", "c", "d" ]
+    p a.my_join         # => "abcd"
+    p a.my_join("$")    # => "a$b$c$d"
+
+    # Write a method that returns a new array containing all the elements of 
+    # the original array in reverse order.
+
+    puts "-----------------------------------"
+    puts "my_reverse"
+    puts "-----------------------------------"
+    p [ "a", "b", "c" ].my_reverse   #=> ["c", "b", "a"]
+    p [ 1 ].my_reverse               #=> [1]
+end
