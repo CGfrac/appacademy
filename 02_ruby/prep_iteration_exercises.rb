50ed8c6e31913c5fcd32239697bf5f5fe40c93a8
CGfrac <cgfrac@protonmail.com>
2020-11-10 10:32:07 +0100

    Refactor Array#bubble_sort! to use blocks

--- 02_ruby/prep_iteration_exercises.rb
+++ 02_ruby/prep_iteration_exercises.rb
@@ -50,11 +50,12 @@ end
 
 class Array
   def bubble_sort!(&prc)
+    prc ||= Proc.new { |num1, num2| num1 <=> num2 }
     sorted = false
     while !sorted
       sorted = true
       (0...self.length-1).each do |i|
-        if self[i] > self[i+1]
+        if prc.call(self[i], self[i+1]) == 1
           sorted = false
           self[i], self[i+1] = self[i+1], self[i]
         end
