e4862eb6bfb442163fb85db944adbe398866c2cf
CGfrac <cgfrac@protonmail.com>
2020-11-11 11:11:35 +0100

    Add test code and refactor output

--- 02_ruby/02_ghost/game.rb
+++ 02_ruby/02_ghost/game.rb
@@ -1,10 +1,11 @@
+require "set"
 require_relative "player.rb"
 
 class Game
     ALPHABET = ('a'..'z').to_a
     GHOST = "GHOST"
 
-    def initialize(player_names)
+    def initialize(player_names=["Player1", "Player2"])
         @players = []
         player_names.each { |name| @players << Player.new(name) }
         @losses = {}
@@ -58,11 +59,14 @@ class Game
     end
 
     def display_standings
+        hr = "-" * 25
+        puts hr
         puts "Player".ljust(15) + " | " + "Losses"
-        puts "-" * 25
+        puts hr
         @losses.each do |player, count|
             puts "#{player.name}".ljust(15) + " | " + self.record(player)
         end
+        puts hr
     end
 
     def run
@@ -77,4 +81,21 @@ class Game
         end
         puts "#{self.current_player.name} is the last person standing!"
     end
+end
+
+if __FILE__ == $PROGRAM_NAME
+    names = []
+    i = 1
+    while true
+        puts "Type player #{i} name: "
+        names << gets.chomp
+        if i >= 2
+            puts "Add another player? (type 'y'): "
+            yes_no = gets.chomp.downcase
+            break if yes_no != "y"
+        end
+        i += 1
+    end
+    game = Game.new(names)
+    game.run
 end
