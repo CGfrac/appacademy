ddd5b17275c8247f31959123088c265cbe0b9a53
CGfrac <cgfrac@protonmail.com>
2020-11-11 09:07:35 +0100

    Implement Game#current_player, previous_player and next_player!

--- 02_ruby/02_ghost/game.rb
+++ 02_ruby/02_ghost/game.rb
@@ -4,4 +4,16 @@ class Game
         @fragment = ""
         @dictionary = File.read("dictionary.txt").split.to_set
     end
+
+    def current_player
+        @players[0]
+    end
+
+    def previous_player
+        @players[-1]
+    end
+
+    def next_player!
+        @players.rotate!
+    end
 end
