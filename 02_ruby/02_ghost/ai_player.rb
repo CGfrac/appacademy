21d3a19ece9df945c54e0cb64126a2f0ec4ed989
CGfrac <cgfrac@protonmail.com>
2020-11-12 10:25:52 +0100

    Make AiPlayer take the best moves available

--- 02_ruby/02_ghost/ai_player.rb
+++ 02_ruby/02_ghost/ai_player.rb
@@ -2,8 +2,8 @@ require_relative "game.rb"
 
 class AiPlayer < Player
     def guess(fragment, n)
-        winning_moves = []
-        losing_moves = []
+        winning_moves = Hash.new(0)
+        losing_moves = Hash.new(0)
 
         Game::ALPHABET.each do |char|
             candidate = fragment + char
@@ -11,18 +11,40 @@ class AiPlayer < Player
                 if word.start_with?(candidate)
                     distance = word.length - candidate.length
                     if distance > 1 && distance <= n
-                        winning_moves << char
+                        winning_moves[char] += 1
                     else
-                        losing_moves << char
+                        losing_moves[char] += 1
                     end
                 end
             end
         end
 
         if winning_moves.length > 0
-            winning_moves.sample
+            candidates = winning_moves.keys
         else
-            losing_moves.sample
+            candidates = losing_moves.keys
         end
+
+        strong_moves = []
+
+        candidates.each do |char|
+            if strong_moves.length > 0
+                best = strong_moves[0]
+            else
+                strong_moves = [char]
+                next
+            end
+
+            best_count = winning_moves[best] - losing_moves[best]
+            count = winning_moves[char] - losing_moves[char]
+
+            if best_count < count
+                strong_moves = [char]
+            elsif best_count == count
+                strong_moves << char
+            end
+        end
+
+        strong_moves.sample
     end
 end
