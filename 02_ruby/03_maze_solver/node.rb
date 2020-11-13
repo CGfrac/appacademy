5e5141de9b4dc50591e58815e35ab998d2f64a99
CGfrac <cgfrac@protonmail.com>
2020-11-13 13:38:56 +0100

    Implement necessary accessors

--- 02_ruby/03_maze_solver/node.rb
+++ 02_ruby/03_maze_solver/node.rb
@@ -1,11 +1,16 @@
 class Node
-    attr_reader :f
+    attr_reader :g, :f
     
     def initialize(coordinates, parent=nil, g=0, h=0)
         @parent = parent
         @coordinates = coordinates
         @g = g
         @h = h
+        @f = @g + @h
+    end
+
+    def @g=(value)
+        @g = value
         @f = @g + @h
     end
 end
