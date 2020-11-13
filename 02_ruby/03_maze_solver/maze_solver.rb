fe86792e38808a209aad49d40111e0c9699b3d1d
CGfrac <cgfrac@protonmail.com>
2020-11-13 14:43:33 +0100

    First attempt at A* algorithm with binary heap

--- 02_ruby/03_maze_solver/maze_solver.rb
+++ 02_ruby/03_maze_solver/maze_solver.rb
@@ -10,25 +10,86 @@ def find_element(maze, char)
     end
 end
 
+# See http://archive.gamedev.net/archive/reference/articles/article2003.html for the underlying logic
 def a_star(maze)
-    a = find_element(maze, 'S')
-    b = find_element(maze, 'E')
-    open = [Node.new(a)]
-    
-    while open.length > 0
+    open = Array.new(maze.length) { Array.new(maze[0].length, false) }
+    closed = Array.new(maze.length) { Array.new(maze[0].length, false) }
+
+    start_x, start_y = find_element(maze, 'S')
+    open[start_x][start_y] = true
+    open_count = 1
+
+    target = find_element(maze, 'E')
+
+    current = Node.new([start_x, start_y], target)
+    binary_heap = [current]
+    addresses = {}
+
+    moves = [
+        [-1,0], [0,-1], [1,0], [0,1], # straights
+        [-1,-1], [1,1], [1,-1], [1,1]  # diagonals
+    ]
+
+    while open_count > 0
         # look for lowest F in open list
-        # switch it to closed list
-            # break if current's coordinates == b
+        binary_heap[0], binary_heap[-1] = binary_heap[-1], binary_heap[0]
+        current = binary_heap.pop
+        index = 1
+        while (index * 2) <= binary_heap.length
+            a = index-1
+            b = (index * 2) - 1
+            if binary_heap[a].f > binary_heap[b].f
+                binary_heap[a], binary_heap[b] = binary_heap[b], binary_heap[a]
+            end
+            index *= 2
+        end
+
+        x, y = current.coordinates
+
+        open[x][y] = false
+        open_count -= 1
+        closed[x][y] = true
+
+        break if maze[x][y] == 'E'
+
         # check all 8 adjacent squares
-            # ignore if in closed list or unwalkable
-            # if not in open list:
-                # set current node as parent
-                # add it with its own G, H and F values
-            # if in open list:
-                # see if G score from current is lower
-                # if it is, change parent to current, update G/F (sort open here again if open is sorted)
+        moves.each_with_index do |move, index|
+            i = move[0] + x
+            j = move[1] + y
+            g = index < 4 ? 10 : 14
+            if closed[i][j] || maze[i][j] == '*'
+                next
+            elsif open[i][j]
+                node = addresses["#{i}:#{j}"]
+                if g < node.g
+                    node.parent = current
+                    node.g = g
+                end
+            else
+                binary_heap << Node.new([i,j], target, current, g)
+                addresses["#{i}:#{j}"] = binary_heap[-1]
+                open[i][j] = true
+                open_count += 1
+            end
+            # sort binary_heap according to F
+            index = binary_heap.length
+            while (index / 2) >= 1
+                a = (index / 2) - 1
+                b = index - 1
+                if binary_heap[a].f > binary_heap[b].f
+                    binary_heap[a], binary_heap[b] = binary_heap[b], binary_heap[a]
+                end
+                index /= 2
+            end
+        end
     end
     # work backward from closed[-1] to start and register path
+    current = current.parent
+    while current.parent
+        x, y = current.coordinates
+        maze[x][y] = 'X'
+        current = current.parent
+    end
 end
 
 if ARGV.length != 1
