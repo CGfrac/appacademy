714624ad2c27562711a5653419b08d1ab419c0c4
CGfrac <cgfrac@protonmail.com>
2020-11-16 09:18:40 +0100

    Implement base class functionality

--- 02_ruby/04_eight_queens_puzzle/eight_queens_puzzle.rb
+++ 02_ruby/04_eight_queens_puzzle/eight_queens_puzzle.rb
@@ -3,3 +3,24 @@
 # sort by conflicts
 # move biggest conflicts to least collisions column
 # repeat 2 previous steps until there is no conflict
+
+class EightQueensBoard
+    def initialize
+        @board = Array.new(8) { Array.new(8, '_') }
+    end
+
+    def populate!
+        (0..7).each do |row|
+            index = rand(0..7)
+            @board[row][index] = 'Q'
+            # check conflicts here
+        end
+    end
+
+    def print
+        @board.each { |row| puts row.join(' ') }
+    end
+end
+
+board = EightQueensBoard.new
+board.print
