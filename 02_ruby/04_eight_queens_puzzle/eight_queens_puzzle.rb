d60d1e793a9dc2c4ccac10b5a71b88f24114af95
CGfrac <cgfrac@protonmail.com>
2020-11-16 09:03:29 +0100

    Initialize folder and write down ideas in comments

---
+++ 02_ruby/04_eight_queens_puzzle/eight_queens_puzzle.rb
@@ -0,0 +1,5 @@
+# populate row by row in a random column each time
+# at each placement check conflicts
+# sort by conflicts
+# move biggest conflicts to least collisions column
+# repeat 2 previous steps until there is no conflict
