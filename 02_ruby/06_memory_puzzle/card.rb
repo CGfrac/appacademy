1b566107df826319aa8eeef1864f60e281c9a672
CGfrac <cgfrac@protonmail.com>
2020-11-17 10:26:45 +0100

    Implement Card #hide and #reveal

--- 02_ruby/06_memory_puzzle/card.rb
+++ 02_ruby/06_memory_puzzle/card.rb
@@ -3,4 +3,12 @@ class Card
         @face_value = face_value
         @face_up = false
     end
+
+    def hide
+        @face_up = false
+    end
+
+    def reveal
+        @face_up = true
+    end
 end
