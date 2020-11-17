cb02d9229b397fb0cd54441f98304de7ae344524
CGfrac <cgfrac@protonmail.com>
2020-11-17 13:40:21 +0100

    Implement Game#check_guess

--- 02_ruby/06_memory_puzzle/game.rb
+++ 02_ruby/06_memory_puzzle/game.rb
@@ -9,4 +9,17 @@ class Game
     def over?
         @board.won?
     end
+
+    def check_guess(guessed_pos)
+        current_guess = @board.reveal(guessed_pos)
+        if @previous_guess
+            unless current_guess == @previous_guess
+                current_guess.hide
+                @previous_guess.hide
+            end
+            @previous_guess = nil
+        else
+            @previous_guess = current_guess
+        end
+    end
 end
