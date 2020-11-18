915bb5ff35b5a3125443ce38ed47249e267258ee
CGfrac <cgfrac@protonmail.com>
2020-11-18 08:27:55 +0100

    Implement ComputerPlayer#get_input and related instance methods

--- 02_ruby/06_memory_puzzle/computer_player.rb
+++ 02_ruby/06_memory_puzzle/computer_player.rb
@@ -1,7 +1,12 @@
 class ComputerPlayer
-    def initialize
+    def initialize(board_width)
+        positions = []
+        (0..board_width).each do |i|
+            (0..board_width).each { |j| positions << [i, j] }
+        end
         @known_cards = {}
         @matched_cards = []
+        @next_input = nil
     end
 
     def receive_card(pos, value)
@@ -10,5 +15,21 @@ class ComputerPlayer
 
     def receive_match(pos1, pos2)
         @matched_cards << [pos1, pos2]
+    end
+
+    def get_input
+        input = nil
+        if @next_input
+            input = @next_input[0..1]
+            @next_input = nil
+        elsif @matched_cards.length > 0
+            matched = @matched_cards.pop
+            input = matched.pop
+            @next_input = matched.pop
+        else
+            index = rand(0...positions.length)
+            input = positions[index]
+        end
+        input
     end
 end
