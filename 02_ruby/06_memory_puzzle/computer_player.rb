dd50e455f01cc5d6776e8d0fd39ffdce27b59baa
CGfrac <cgfrac@protonmail.com>
2020-11-18 08:12:06 +0100

    Implement ComputerPlayer#receive_match

--- 02_ruby/06_memory_puzzle/computer_player.rb
+++ 02_ruby/06_memory_puzzle/computer_player.rb
@@ -1,10 +1,14 @@
 class ComputerPlayer
     def initialize
         @known_cards = {}
-        @matched_cards = {}
+        @matched_cards = []
     end
 
     def receive_card(pos, value)
         @known_cards[value] = pos
+    end
+
+    def receive_match(pos1, pos2)
+        @matched_cards << [pos1, pos2]
     end
 end
