5cf2aff4b4ee0f7639d57bca759d2f899b7113f4
CGfrac <cgfrac@protonmail.com>
2020-11-19 08:54:54 +0100

    Implement Board::from_file

--- 02_ruby/07_sudoku/board.rb
+++ 02_ruby/07_sudoku/board.rb
@@ -1,4 +1,24 @@
+require_relative "tile.rb"
+
 class Board
+    def self.from_file(path)
+        file = File.open(path)
+        lines = file.readlines.map(&:chomp)
+        grid = []
+        lines.each do |line|
+            row = []
+            line.split("").each do |value|
+                if value.to_i == 0
+                    row << Tile.new(' ', false)
+                else
+                    row << Tile.new(value, true)
+                end
+            end
+            grid << row
+        end
+        grid
+    end
+
     def initialize(grid)
         @grid = grid
     end
