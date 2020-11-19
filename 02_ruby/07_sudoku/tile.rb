6dd5c16e1ce6af3a7e83f0fbc17100e34f574c68
CGfrac <cgfrac@protonmail.com>
2020-11-19 08:29:38 +0100

    Implement Tile#to_s

--- 02_ruby/07_sudoku/tile.rb
+++ 02_ruby/07_sudoku/tile.rb
@@ -1,6 +1,16 @@
+require "colorize"
+
 class Tile
     def initialize(value, given)
         @value = value
         @given = given
+    end
+
+    def to_s
+        if @given
+            @value.light_green
+        else
+            @value
+        end
     end
 end
