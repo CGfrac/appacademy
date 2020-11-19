3ad3277d0222e71195815864fc2aeef1c23ddbe9
CGfrac <cgfrac@protonmail.com>
2020-11-19 16:51:11 +0100

    Optimize solution

--- 02_ruby/05_project_euler/003_largest_prime_factor.rb
+++ 02_ruby/05_project_euler/003_largest_prime_factor.rb
@@ -1,15 +1,28 @@
-number = 600851475143
-n = 3
+n = 600851475143
 
-def is_prime?(n)
-    (3..Integer.sqrt(n)).each { |i| return false if n % i == 0 }
-    true
+if n.even?
+    last_factor = 2
+    n /= 2 while n.even?
+else
+    last_factor = 1
 end
 
-while true
-    break if n == number
-    number /= n if number % n == 0 && is_prime?(n)
-    n += 2
+factor = 3
+max_factor = Integer.sqrt(n)
+
+while n > 1 && factor <= max_factor
+    if n % factor == 0
+        last_factor = factor
+        n /= factor while n % factor == 0
+        max_factor = Integer.sqrt(n)
+    end
+    factor += 2
 end
 
-puts n
+if n == 1
+    puts last_factor
+else
+    puts n
+end
+
+    
