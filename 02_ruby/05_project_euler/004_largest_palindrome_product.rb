a9bd186781793740250a0a6354f5bbdb11a9ad90
CGfrac <cgfrac@protonmail.com>
2020-11-25 11:36:11 +0100

    Refactor solution

--- 02_ruby/05_project_euler/004_largest_palindrome_product.rb
+++ 02_ruby/05_project_euler/004_largest_palindrome_product.rb
@@ -1,21 +1,23 @@
-max = 0
+largest_palindrome = 0
+
+def reverse(num)
+    reversed = 0
+    while num > 0
+        reversed = reversed * 10 + num % 10
+        num /= 10
+    end
+    reversed
+end
 
 def palindrome?(num)
-    reverse = 0
-    helper = num
-    while helper > 0
-        reverse *= 10
-        reverse += helper % 10
-        helper /= 10
-    end
-    num == reverse
+    num == reverse(num)
 end
 
 (100..999).each do |a|
     (a..999).each do |b|
         product = a * b
-        max = [max, product].max if palindrome?(product)
+        largest_palindrome = [largest_palindrome, product].max if palindrome?(product)
     end
 end
 
-puts max
+puts largest_palindrome
