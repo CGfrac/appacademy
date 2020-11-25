c2e677aa5de8ee99330852474148c3bf52669d7c
CGfrac <cgfrac@protonmail.com>
2020-11-25 11:47:13 +0100

    Optimize nested loop further

--- 02_ruby/05_project_euler/004_largest_palindrome_product.rb
+++ 02_ruby/05_project_euler/004_largest_palindrome_product.rb
@@ -15,12 +15,19 @@ end
 
 a = 999
 while a >= 100
-    b = 999
+    # Either a or b must have a factor of 11
+    if a % 11 == 0
+        b = 999
+        db = 1
+    else
+        b = 990
+        db = 11
+    end
     while b >= a
         product = a * b
         break if product <= largest_palindrome
         largest_palindrome = [largest_palindrome, product].max if palindrome?(product)
-        b -= 1
+        b -= db
     end
     a -= 1
 end
