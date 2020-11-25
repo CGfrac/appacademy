b5af9b1c0bca57addad33cd0d211a5f5ac8514ab
CGfrac <cgfrac@protonmail.com>
2020-11-25 10:34:02 +0100

    Add more useful links for later use

--- 02_ruby/07_sudoku/solver.rb
+++ 02_ruby/07_sudoku/solver.rb
@@ -1,4 +1,8 @@
 # refer to https://en.wikipedia.org/wiki/Sudoku_solving_algorithms
+# https://en.wikipedia.org/wiki/Exact_cover#Sudoku
+# https://gieseanw.wordpress.com/2011/06/16/solving-sudoku-revisited/
+# https://en.wikipedia.org/wiki/Knuth%27s_Algorithm_X
+# https://en.wikipedia.org/wiki/Dancing_Links
 
 class Solver
 end
