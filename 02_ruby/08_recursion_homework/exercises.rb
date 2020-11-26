2b2c4729dec51df6c39d76fedc17d36f96fc5d2a
CGfrac <cgfrac@protonmail.com>
2020-11-26 11:32:18 +0100

    Implement add_numbers

--- 02_ruby/08_recursion_homework/exercises.rb
+++ 02_ruby/08_recursion_homework/exercises.rb
@@ -17,3 +17,21 @@ p sum_to(5)  # => returns 15
 p sum_to(1)  # => returns 1
 p sum_to(9)  # => returns 45
 p sum_to(-8)  # => returns nil
+
+# Write a function add_numbers(nums_array) that takes in an array 
+# of Integers and returns the sum of those numbers. 
+# Write this method recursively.
+
+def add_numbers(nums_array)
+    return nums_array.first if nums_array.length <= 1
+    nums_array.first + add_numbers(nums_array[1..-1])
+end
+
+# Test Cases
+puts hr
+puts "add_numbers"
+puts hr
+p add_numbers([1,2,3,4]) # => returns 10
+p add_numbers([3]) # => returns 3
+p add_numbers([-80,34,7]) # => returns -39
+p add_numbers([]) # => returns nil
