1236c0c39153fe581309012547a8f652db2eea84
CGfrac <cgfrac@protonmail.com>
2020-11-26 11:42:23 +0100

    Implement gamma_fnc

--- 02_ruby/08_recursion_homework/exercises.rb
+++ 02_ruby/08_recursion_homework/exercises.rb
@@ -35,3 +35,21 @@ p add_numbers([1,2,3,4]) # => returns 10
 p add_numbers([3]) # => returns 3
 p add_numbers([-80,34,7]) # => returns -39
 p add_numbers([]) # => returns nil
+
+# Let's write a method that will solve Gamma Function recursively. 
+# The Gamma Function is defined Γ(n) = (n-1)!.
+
+def gamma_fnc(n)
+    return 1 if n == 1
+    return nil if n <= 0
+    (n - 1) * gamma_fnc(n - 1)
+end
+
+# Test Cases
+puts hr
+puts "gamma_fnc"
+puts hr
+p gamma_fnc(0)  # => returns nil
+p gamma_fnc(1)  # => returns 1
+p gamma_fnc(4)  # => returns 6
+p gamma_fnc(8)  # => returns 5040
