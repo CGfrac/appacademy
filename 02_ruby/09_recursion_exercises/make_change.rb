754c6202e5aceceb0f6898b485d54c4e07ac294c
CGfrac <cgfrac@protonmail.com>
2020-11-28 11:37:12 +0100

    Implement greedy_make_change

--- 02_ruby/09_recursion_exercises/make_change.rb
+++ 02_ruby/09_recursion_exercises/make_change.rb
@@ -1,3 +1,12 @@
-def make_change(amount, coins = [25, 10, 5, 1])
-    
+def greedy_make_change(amount, coins = [25, 10, 5, 1])
+    return [] if amount == 0
+    change = []
+    i = 0
+    i += 1 while coins[i] > amount
+    coin = coins[i]
+    change << coin
+    change += greedy_make_change(amount - coin, coins)
+    change
 end
+
+p greedy_make_change(39) # [25, 10, 1, 1, 1, 1]
