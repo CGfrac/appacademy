0c210690a99963816bfbf0cf066ff7c1e97010c4
CGfrac <cgfrac@protonmail.com>
2020-11-29 10:43:52 +0100

    Implement adjacent_words

--- 02_ruby/10_word_chains/word_chain.rb
+++ 02_ruby/10_word_chains/word_chain.rb
@@ -1,7 +1,26 @@
 require "set"
 
 class WordChainer
+    ALPHABET = ('a'..'z').to_a
+
     def initialize(dictionary_file_name)
-        @dictionary = File.open(dictionary_file_name).split.to_set
+        @dictionary = File.read(dictionary_file_name).split.to_set
+    end
+
+    def adjacent_words(word)
+        adjacent = []
+
+        (0...word.length).each do |i|
+            candidate = word[0..-1]
+
+            ALPHABET.each do |char|
+                candidate[i] = char
+                if @dictionary.include?(candidate) && candidate != word
+                    adjacent << candidate[0..-1]
+                end
+            end
+        end
+
+        adjacent
     end
 end
