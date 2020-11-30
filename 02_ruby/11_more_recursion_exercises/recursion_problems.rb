a8b3186b97cca3c29daf35b803538940a9cfe8f4
CGfrac <cgfrac@protonmail.com>
2020-11-30 09:06:11 +0100

    Prepare format for output

--- 02_ruby/11_more_recursion_exercises/recursion_problems.rb
+++ 02_ruby/11_more_recursion_exercises/recursion_problems.rb
@@ -1,29 +1,48 @@
+def print_problem_name(name)
+    hr = '-' * 20
+    puts hr
+    puts name
+    puts hr
+end
+
 #Problem 1: 
 
 def sum_recur(array)
 end
+
+print_problem_name("sum_recur")
 
 #Problem 2: 
 
 def includes?(array, target)
 end
 
+print_problem_name("includes?")
+
 # Problem 3: 
 
 def num_occur(array, target)
 end
+
+print_problem_name("num_occur")
 
 # Problem 4: 
 
 def add_to_twelve?(array)
 end
 
+print_problem_name("add_to_twelve?")
+
 # Problem 5: 
 
 def sorted?(array)
 end
 
+print_problem_name("sorted?")
+
 # Problem 6: 
 
 def reverse(string)
 end
+
+print_problem_name("reverse")
