483f7f1efa1f7c6b3196fbd3b57843c2ea5bed60
CGfrac <cgfrac@protonmail.com>
2020-11-30 09:13:04 +0100

    Implement sum_recur

--- 02_ruby/11_more_recursion_exercises/recursion_problems.rb
+++ 02_ruby/11_more_recursion_exercises/recursion_problems.rb
@@ -8,9 +8,12 @@ end
 #Problem 1: 
 
 def sum_recur(array)
+    return 0 if array.length == 0
+    array[0] + sum_recur(array[1..-1])
 end
 
 print_problem_name("sum_recur")
+p sum_recur([16, 16, 5, 7, 13, 8, 19, 17, 0, 8]) # 109
 
 #Problem 2: 
 
