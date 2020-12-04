1dd043adc5fb70436c1db9cd8a7e30d371a06aa1
CGfrac <cgfrac@protonmail.com>
2020-12-04 11:25:56 +0100

    implement set_bomb_coords and refactor initialize

--- 02_ruby/12_minesweeper/board.rb
+++ 02_ruby/12_minesweeper/board.rb
@@ -1,10 +1,17 @@
+require "set"
 require_relative "tile.rb"
 
 class Board
     def initialize
-        @grid = populate_board
+        @grid = Array.new(9) { Array.new(9) }
+        populate_grid
     end
 
-    def populate_board
+    def set_bomb_coords
+        bombs = Set.new
+        while bombs.length < 12
+            bombs << [rand(0...9), rand(0...9)]
+        end
+        bombs
     end
 end
