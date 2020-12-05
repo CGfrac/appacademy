f2d38815834a2cc2346f3bb83bf77d73050cb80a
CGfrac <cgfrac@protonmail.com>
2020-12-05 10:01:40 +0100

    Implement neighbors method

--- 02_ruby/12_minesweeper/tile.rb
+++ 02_ruby/12_minesweeper/tile.rb
@@ -1,11 +1,35 @@
 require_relative "board.rb"
 
 class Tile
-    def initialize(board, coord, bombed)
+    DELTA = [
+        [-1, -1],
+        [-1,  0],
+        [-1,  1],
+        [ 0,  1],
+        [ 1,  1],
+        [ 1,  0],
+        [ 1, -1],
+        [ 0, -1]
+    ]
+
+    def initialize(board, pos, bombed)
         @board = board
-        @coord = coord
+        @pos = pos
         @bombed = bombed
         @flagged = false
         @revealed = false
+    end
+
+    def neighbors
+        x, y = @pos
+        neighbors = []
+        DELTA.each do |d_x, d_y|
+            n_x = x + d_x
+            n_y = y + d_y
+            if 0 <= n_x && n_x < @board.height && 0 <= n_y && n_y < @board.width
+                neighbors << @board[n_x, n_y]
+            end
+        end
+        neighbors
     end
 end
