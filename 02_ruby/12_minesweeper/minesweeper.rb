560112a26fc2553b01e1e41a0c6171854f9bd207
CGfrac <cgfrac@protonmail.com>
2020-12-06 10:04:49 +0100

    Implement get_input

--- 02_ruby/12_minesweeper/minesweeper.rb
+++ 02_ruby/12_minesweeper/minesweeper.rb
@@ -8,4 +8,18 @@ class Minesweeper
     def over?
         @board.remaining_tiles == 0
     end
+
+    def get_input
+        puts "Enter a command ('r' to reveal, 'f' for flagging)"
+        puts "Followed by the coordinates, e.g. 'r 2,3'"
+        until input
+            begin
+                input = gets.chomp.split
+                command, pos = parse_input(input)
+            rescue
+                input = nil
+            end
+        end
+        input
+    end
 end
