03cd493054ab8379fdc6680cdf236534d42f882c
CGfrac <cgfrac@protonmail.com>
2020-12-06 10:31:03 +0100

    Implement get_command and begin get_input refactor

--- 02_ruby/12_minesweeper/minesweeper.rb
+++ 02_ruby/12_minesweeper/minesweeper.rb
@@ -9,13 +9,22 @@ class Minesweeper
         @board.remaining_tiles == 0
     end
 
+    def get_command
+        cmd = nil
+        until cmd
+            print "Enter a command ('r' to reveal, 'f' for flagging): "
+            input = gets.chomp
+            cmd = input if valid_command?(input)
+        end
+    end
+
     def get_input
-        puts "Enter a command ('r' to reveal, 'f' for flagging)"
         puts "Followed by the coordinates, e.g. 'r 2,3'"
+        input = nil
         until input
             begin
-                input = gets.chomp.split
-                command, pos = parse_input(input)
+                command = get_command
+                position = get_position
             rescue
                 input = nil
             end
