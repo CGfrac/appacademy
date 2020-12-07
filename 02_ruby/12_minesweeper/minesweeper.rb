3b0a092442527851952dfe4eddb3502aec847eff
CGfrac <cgfrac@protonmail.com>
2020-12-07 11:39:10 +0100

    Clean code for new input handling

--- 02_ruby/12_minesweeper/minesweeper.rb
+++ 02_ruby/12_minesweeper/minesweeper.rb
@@ -1,10 +1,7 @@
 require "yaml"
-require "set"
 require_relative "board.rb"
 
 class Minesweeper
-    VALID_COMMANDS = Set['r', 'f', 's', 'q']
-
     def initialize
         @board = Board.new
     end
@@ -13,54 +10,10 @@ class Minesweeper
         @board.remaining_tiles == 0 || @boom
     end
 
-    def valid_command?(cmd)
-        VALID_COMMANDS.include?(cmd)
-    end
-
     def save_game
         print "Give your save a name: "
         input = gets.chomp
         File.write(input, self.to_yaml)
-    end
-
-    def get_command
-        cmd = nil
-        until cmd
-            print "Enter a command ('r' to reveal, 'f' for flagging, 's' to save, 'q' to quit): "
-            input = gets.chomp
-            cmd = input if self.valid_command?(input)
-            if cmd == 's'
-                self.save_game
-                cmd = nil
-            elsif cmd == 'q'
-                exit
-            end
-        end
-        cmd
-    end
-
-    def valid_position?(pos)
-        pos.length == 2 && pos[0].between?(0, @board.height-1) && pos[1].between?(0, @board.width-1)
-    end
-
-    def get_position
-        pos = nil
-        until pos
-            print "Enter coordinates in 'row,col' form (e.g. '2,3'): "
-            begin 
-                input = gets.chomp.split(',').map { |coord| Integer(coord) }
-                pos = input if self.valid_position?(input)
-            rescue
-                pos = nil
-            end
-        end
-        pos
-    end
-
-    def get_input
-        command = self.get_command
-        position = self.get_position
-        [command, position]
     end
 
     def reveal_tile(position)
@@ -72,13 +25,11 @@ class Minesweeper
         end
     end
 
+    def prompt
+    end
+
     def play_turn
-        command, position = self.get_input
-        if command == 'f'
-            @board[*position].switch_flag
-        else
-            self.reveal_tile(position)
-        end
+        self.prompt
     end
 
     def victory_message
@@ -102,6 +53,5 @@ if __FILE__ == $PROGRAM_NAME
     else
         game = Minesweeper.new
     end
-    sleep(5)
     game.run
 end
