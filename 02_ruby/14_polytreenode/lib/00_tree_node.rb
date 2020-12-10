8407ac5de72bb169bd31bc790b6c77ff647f22d8
CGfrac <cgfrac@protonmail.com>
2020-12-10 10:16:08 +0100

    Implement #parent= and make children accessor

--- 02_ruby/14_polytreenode/lib/00_tree_node.rb
+++ 02_ruby/14_polytreenode/lib/00_tree_node.rb
@@ -1,9 +1,16 @@
 class PolyTreeNode
-    attr_reader :value, :parent, :children
+    attr_reader :value, :parent
+    attr_accessor :children
 
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
+    end
+
+    def parent=(node)
+        @parent.children.reject! { |child| child == self } if @parent
+        @parent = node
+        node.children << self if node
     end
 end
