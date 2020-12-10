4eddc7eb64ed8901c0e055daa319c11626b6d2ad
CGfrac <cgfrac@protonmail.com>
2020-12-10 10:31:15 +0100

    Implement #add_child and #remove_child

--- 02_ruby/14_polytreenode/lib/00_tree_node.rb
+++ 02_ruby/14_polytreenode/lib/00_tree_node.rb
@@ -8,9 +8,18 @@ class PolyTreeNode
         @children = []
     end
 
-    def parent=(node)
+    def parent=(parent_node)
         @parent.children.reject! { |child| child == self } if @parent
-        @parent = node
-        node.children << self if node
+        @parent = parent_node
+        parent_node.children << self if parent_node
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "Not a child" unless @children.include?(child_node)
+        child_node.parent = nil
     end
 end
