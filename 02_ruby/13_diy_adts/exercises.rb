69a3d28f0c824afbd56beff22fd438b57ce90aca
CGfrac <cgfrac@protonmail.com>
2020-12-10 09:42:58 +0100

    Implement queue

--- 02_ruby/13_diy_adts/exercises.rb
+++ 02_ruby/13_diy_adts/exercises.rb
@@ -19,3 +19,21 @@ class Stack
         @ivar[-1]
     end
 end
+
+class Queue
+    def initialize
+        @ivar = []
+    end
+
+    def enqueue(el)
+        @ivar << el
+    end
+
+    def dequeue
+        @ivar.shift
+    end
+
+    def peek
+        @ivar[0]
+    end
+end
