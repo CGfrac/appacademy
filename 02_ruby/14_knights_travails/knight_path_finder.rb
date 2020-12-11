be96a5f61254b4463894914e06774721e84bbc7b
CGfrac <cgfrac@protonmail.com>
2020-12-11 10:21:40 +0100

    Refactor valid_moves

--- 02_ruby/14_knights_travails/knight_path_finder.rb
+++ 02_ruby/14_knights_travails/knight_path_finder.rb
@@ -3,26 +3,26 @@ require_relative "lib/00_tree_node.rb"
 
 class KnightPathFinder
 
+    MOVES = [
+        [-2,-1], [-2, 1],
+        [ 2,-1], [ 2, 1],
+        [-1,-2], [ 1,-2],
+        [-1, 2], [ 1, 2]
+    ]
+
     def self.valid_moves(pos)
-        row, col = pos
-        moves = []
-        if row - 2 >= 0
-            moves << [row - 2, col - 1] if col - 1 >= 0
-            moves << [row - 2, col + 1] if col + 1 < 8
+        valid_moves = []
+        x, y = pos
+
+        MOVES.each do |move|
+            d_x, d_y = move
+            new_pos = [x + d_x, y + d_y]
+            if new_pos.all? { |coord| coord.between?(0,7) }
+                valid_moves << new_pos
+            end
         end
-        if row + 2 < 8
-            moves << [row + 2, col - 1] if col - 1 >= 0
-            moves << [row + 2, col + 1] if col + 1 < 8
-        end
-        if col - 2 >= 0
-            moves << [row - 1, col - 2] if row - 1 >= 0
-            moves << [row + 1, col - 2] if row + 1 < 8
-        end
-        if col + 2 < 8
-            moves << [row - 1, col + 2] if row - 1 >= 0
-            moves << [row + 1, col + 2] if row + 1 < 8
-        end
-        moves
+
+        valid_moves
     end
 
     def initialize(starting_pos)
