d02ff8c2ab6608597ac87a612f1c1ae414ce7a5f
CGfrac <cgfrac@protonmail.com>
2020-12-11 09:38:02 +0100

    Implement valid_moves

--- 02_ruby/14_knights_travails/knight_path_finder.rb
+++ 02_ruby/14_knights_travails/knight_path_finder.rb
@@ -1,6 +1,29 @@
 require_relative "lib/00_tree_node.rb"
 
 class KnightPathFinder
+
+    def self.valid_moves(pos)
+        row, col = pos
+        moves = []
+        if row - 2 >= 0
+            moves << [row - 2, col - 1] if col - 1 >= 0
+            moves << [row - 2, col + 1] if col + 1 < 8
+        end
+        if row + 2 < 8
+            moves << [row + 2, col - 1] if col - 1 >= 0
+            moves << [row + 2, col + 1] if col + 1 < 8
+        end
+        if col - 2 >= 0
+            moves << [row - 1, col - 2] if row - 1 >= 0
+            moves << [row + 1, col - 2] if row + 1 < 8
+        end
+        if col + 2 < 8
+            moves << [row - 1, col + 2] if row - 1 >= 0
+            moves << [row + 1, col + 2] if row + 1 < 8
+        end
+        moves
+    end
+
     def initialize(starting_pos)
         @root_node = PolyTreeNode(starting_pos)
     end
