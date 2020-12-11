5457bf090b5fa0fb760d751864edfce3247b3f05
CGfrac <cgfrac@protonmail.com>
2020-12-11 11:17:05 +0100

    Implement children

--- 02_ruby/15_tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ 02_ruby/15_tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -16,5 +16,17 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children = []
+    @board.each_with_index do |row, i|
+      row.each_with_index do |col, j|
+        if @board[i,j].empty?
+          board_duplicate = @board.dup.map!(&:dup)
+          board_duplicate[i,j] = @next_mover_mark
+          next_mark = @next_mover_mark == :o ? :x : :o
+          children << TicTacToeNode.new(board_duplicate, next_mark, [i, j])
+        end
+      end
+    end
+    children
   end
 end
