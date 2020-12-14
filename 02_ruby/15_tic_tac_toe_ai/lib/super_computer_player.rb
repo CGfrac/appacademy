d6b74d9c9a22586a9a16ee637956d53ec7ba3dac
CGfrac <cgfrac@protonmail.com>
2020-12-14 10:38:38 +0100

    Implement move

--- 02_ruby/15_tic_tac_toe_ai/lib/super_computer_player.rb
+++ 02_ruby/15_tic_tac_toe_ai/lib/super_computer_player.rb
@@ -2,6 +2,10 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    node.children.each { |child| return child.prev_move_pos if child.winning_node?(mark) }
+    node.children.each { |child| return child.prev_move_pos unless child.losing_node?(mark) }
+    raise "Error: there should always be a non-losing move."
   end
 end
 
