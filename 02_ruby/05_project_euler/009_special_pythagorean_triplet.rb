7102b7451a3f6c91993b08f318ceafef571fa125
CGfrac <cgfrac@protonmail.com>
2020-12-15 17:05:45 +0100

    Implement solution

---
+++ 02_ruby/05_project_euler/009_special_pythagorean_triplet.rb
@@ -0,0 +1,45 @@
+# Inspired from https://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples
+matrices = [
+    [
+        [1, -2, 2],
+        [2, -1, 2],
+        [2, -2, 3]
+    ],
+    [
+        [1, 2, 2],
+        [2, 1, 2],
+        [2, 2, 3]
+    ],
+    [
+        [-1, 2, 2],
+        [-2, 1, 2],
+        [-2, 2, 3]
+    ]
+]
+
+def multiply(matrix, vector)
+    new_vector = []
+    matrix.each do |row|
+        product = 0
+        (0...vector.length).each do |index|
+            product += row[index] * vector[index]
+        end
+        new_vector << product
+    end
+    new_vector
+end
+
+i = 1
+while true
+    queue = [[3*i, 4*i, 5*i]]
+    until queue.empty?
+        triple = queue.shift
+        if triple.sum == 1000
+            puts triple[0] * triple[1] * triple[2]
+            exit
+        elsif triple.sum < 1000
+            matrices.each { |matrix| queue << multiply(matrix, triple) }
+        end
+    end
+    i += 1
+end
