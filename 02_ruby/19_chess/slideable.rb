7165f54fe2ebb76401c728720db8f9e9ed614ff9
CGfrac <cgfrac@protonmail.com>
2020-12-19 11:13:53 +0100

    Implement skeleton

--- 02_ruby/19_chess/slideable.rb
+++ 02_ruby/19_chess/slideable.rb
@@ -5,4 +5,20 @@ module Slideable
     DIAGONAL_DIRS = [
         [-1, -1], [-1, 1], [1, -1], [1, 1]
     ]
+
+    def horizontal_dirs
+    end
+
+    def diagonal_dirs
+    end
+
+    def moves
+    end
+
+    private
+    def move_dirs
+    end
+
+    def grow_unblocked_moves_in_dir(dx, dy)
+    end
 end
