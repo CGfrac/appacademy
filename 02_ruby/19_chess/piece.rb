1273d1c995aed911d5795c004d88f19ef4559ca9
CGfrac <cgfrac@protonmail.com>
2020-12-19 11:02:17 +0100

    Put skeleton for class

--- 02_ruby/19_chess/piece.rb
+++ 02_ruby/19_chess/piece.rb
@@ -1,4 +1,7 @@
 class Piece
+    attr_reader :color, :board
+    attr_accessor :pos
+
     def initialize(color, board, pos)
         @color = color
         @board = board
@@ -7,5 +10,18 @@ class Piece
 
     def to_s
         @color.to_s
+    end
+
+    def empty?
+    end
+
+    def valid_moves
+    end
+
+    def Symbol
+    end
+
+    private
+    def move_into_check?(end_pos)
     end
 end
