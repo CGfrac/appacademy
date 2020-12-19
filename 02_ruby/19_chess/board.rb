1fb88cdc8fbebfd5af68cf7c532e1111cdb73e40
CGfrac <cgfrac@protonmail.com>
2020-12-19 09:45:34 +0100

    Initialize board and implement temporary populate method

--- 02_ruby/19_chess/board.rb
+++ 02_ruby/19_chess/board.rb
@@ -1,1 +1,14 @@
 require_relative "piece"
+
+class Board
+    def initialize
+        @board = Array.new(8) { Array.new(8, nil) }
+        populate_board
+    end
+
+    def populate_board
+        (0...2).each do |i| 
+            (0...@board[0].length).each { |j| @board[i][j] = Piece }
+        end
+    end
+end
