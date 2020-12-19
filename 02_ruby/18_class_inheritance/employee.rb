674945b522ba3032f7ac3330a20ac81b6973f72b
CGfrac <cgfrac@protonmail.com>
2020-12-19 09:20:01 +0100

    Change employees logic and add testing code

--- 02_ruby/18_class_inheritance/employee.rb
+++ 02_ruby/18_class_inheritance/employee.rb
@@ -14,12 +14,28 @@ class Employee
 end
 
 class Manager < Employee
-    def initialize(name, title, salary, boss, employees)
-        super(name, title, salary, boss)
-        @employees = employees
+    attr_accessor :employees
+
+    def initialize(name, title, salary, boss)
+        super
+        @employees = []
     end
 
     def bonus(multiplier)
-        @employees.inject { |total, employee| employee.salary } * multiplier
+        @employees.inject(0) { |sum, employee| sum + employee.salary } * multiplier
     end
+end
+
+if __FILE__ == $PROGRAM_NAME
+    ned = Manager.new('Ned', 'Founder', 1_000_000, nil)
+    darren = Manager.new('Darren', 'TA Manager', 48_000, ned)
+    shawna = Employee.new('Shawna', 'TA', 12_000, darren)
+    david = Employee.new('David', 'TA', 10_000, darren)
+
+    darren.employees = [shawna, david]
+    ned.employees = [darren, shawna, david]
+
+    p ned.bonus(5) # => 500_000
+    p darren.bonus(4) # => 88_000
+    p david.bonus(3) # => 30_000
 end
