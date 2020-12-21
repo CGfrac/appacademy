69b55612b3266970afd58c21079354ccdb8a2aee
CGfrac <cgfrac@protonmail.com>
2020-12-21 11:41:50 +0100

    Implement #[]= and further refactor

--- 02_ruby/19_chess/board.rb
+++ 02_ruby/19_chess/board.rb
@@ -11,22 +11,23 @@ class Board
         @board[x][y]
     end
 
+    def []=(pos, val)
+        self[pos] = val
+    end
+
     def populate_board
         (0...2).each do |i| 
-            (0...self[0].length).each { |j| self[i,j] = Piece }
+            (0...self[0].length).each { |j| self[i, j] = Piece }
         end
         (self.length-2...self.length).each do |i|
-            (0...self[0].length).each { |j| self[i,j] = Piece }
+            (0...self[0].length).each { |j| self[i, j] = Piece }
         end
     end
 
     def move_piece(start_pos, end_pos)
-        start_x, start_y = start_pos
-        end_x, end_y = end_pos
+        raise ArgumentError.new "Invalid start position" if self[start_pos] == nil
+        raise ArgumentError.new "Invalid end position" if self[end_pos] == Piece
 
-        raise ArgumentError.new "Invalid start position" if self[start_x,start_y] == nil
-        raise ArgumentError.new "Invalid end position" if self[end_x,end_y] == Piece
-
-        self[end_x,end_y], self[start_x,start_y] = self[start_x,start_y], nil
+        self[end_pos], self[start_pos] = self[start_pos], nil
     end
 end
