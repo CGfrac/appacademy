55d3e1828784aac0ce2b408ba4d5dfef91a51726
CGfrac <cgfrac@protonmail.com>
2020-12-21 11:46:15 +0100

    Add skeleton for reminder of Board implementation

--- 02_ruby/19_chess/board.rb
+++ 02_ruby/19_chess/board.rb
@@ -24,10 +24,34 @@ class Board
         end
     end
 
-    def move_piece(start_pos, end_pos)
+    def move_piece(color, start_pos, end_pos)
         raise ArgumentError.new "Invalid start position" if self[start_pos] == nil
         raise ArgumentError.new "Invalid end position" if self[end_pos] == Piece
 
         self[end_pos], self[start_pos] = self[start_pos], nil
+    end
+
+    def valid_pos?(pos)
+    end
+
+    def add_piece(piece, pos)
+    end
+
+    def checkmate?(color)
+    end
+
+    def in_check?(color)
+    end
+
+    def find_king(color)
+    end
+
+    def pieces
+    end
+
+    def dup
+    end
+
+    def move_piece!(color, start_pos, end_pos)
     end
 end
